51405737acb4c6bebcb6cdeea6b1f06efd9f6c90
Jefferson Vidal <vidalmoc@gmail.com>
2020-04-03 18:08:56 -0300

    Inseridos os comandos no arquivo AppsInstall.sh e inseridas instruções e formatações no README.md. Inserida logo do projeto na pasta screenshots

--- AppsInstall.sh
+++ AppsInstall.sh
@@ -7,31 +7,28 @@ echo "=  Instalador Automático de Programas - By Jefferson Vidal  ="
 echo "============================================================="
 echo "	Atualizar Sistema - 1:"
 echo "	Instalar Pacotes Essenciais - 2:"
-echo "	Instalar MySQL 5.5 - 14.04 LTS - 3:"
-echo "	Instalar MysQL 5.6 - 15.04 ou superior - 4:"
-echo "	Instalar MysQL Workbench - 5:"
-echo "	Instalar PHP5 - 6:"
-echo "	Instalar Wine 1.7 - 7:"
-echo "	Instalar Gimp - 8:"
-echo "	Instalar Simple Screen Recorder (Grava Video) - 9:"
-echo "	Instalar Kdenliver (Editor de Video) - 10:"
-echo "	Instalar Audacity (Grava e Edita Audio) - 11:"
-echo "	Instalar Natron x64 - (Similar ao AfterFX) - 12:"
-echo "	Instalar OpenShot (Efeitos+Editor de Video) - 13:"
-echo "	Instalar Driver de Video nVidia GeForce GT 740M - 14:"
-echo "	Instalar PopCorn Time - (Similar NetFlix Gratuito) - 15:"
-echo "	Instalar RetroArch - Emulador de Games Antigos - 16:"
+echo "	Instalar MySQL Server - 3:"
+echo "	Instalar MySQL Workbench - 4:"
+echo "	Instalar PHP7.0 + Apache2 - 5:"
+echo "	Instalar Wine 1.7 - 6:"
+echo "	Instalar Gimp - 7:"
+echo "	Instalar Simple Screen Recorder (Grava Video) - 8:"
+echo "	Instalar Kdenliver (Editor de Video) - 9:"
+echo "	Instalar Audacity (Grava e Edita Audio) - 10:"
+echo "	Instalar Natron x64 - (Similar ao AfterFX) - 11:"
+echo "	Instalar OpenShot (Efeitos+Editor de Video) - 12:"
+echo "	Instalar Driver de Video nVidia GeForce GT 740M - 13:"
+echo "	Instalar PopCorn Time - (Similar NetFlix Gratuito) - 14:"
+echo "	Instalar RetroArch - Emulador de Games Antigos - 15:"
 echo "============================================================="
 echo "===============	  Para Web Development	    ==============="
 echo "============================================================="
-echo "	Git	- 17:"
-echo "	NodeJS	- 171:"
+echo "	Git	- 16:"
+echo "	NodeJS	- 17:"
 echo "	Composer - 18:"
 echo "	Java 8 - 19:"
 echo "	Codec FFMPEG - 20:"
 echo "	Format Junkie (Converte Video) - 21:"
-echo "	"
-echo "	"
 echo "	"
 echo "	q para sair q:"
 echo -e "\n"
@@ -41,104 +38,89 @@ read answer
 
 case "$answer" in
 
-1) sudo add-apt-repository ppa:ondrej/php5;
-sudo add-apt-repository -y ppa:webupd8team/java;
-sudo apt-get update; sudo apt-get upgrade ;;
+1) sudo apt update; sudo apt upgrade ;;
 
-2) sudo apt-get install build-essential;
-sudo apt-get install module-assistant;
-sudo apt-get install ubuntu-restricted-extras;
-sudo apt-get update ;;
+2) sudo apt install build-essential;
+sudo apt install module-assistant;
+sudo apt update ;;
 
-3) sudo apt-get install mysql-client-5.5 mysql-client-core-5.5 mysql-common mysql-server-5.5 mysql-server-core-5.5;
-sudo apt-get update ;;
+3) sudo apt install mysql-server;
+sudo apt update ;;
 
-4) sudo apt-get install mysql-client-5.6 mysql-client-core-5.6 mysql-common mysql-server-5.6 mysql-server-core-5.6;
-sudo apt-get update ;;
+4) sudo apt install mysql-workbench mysql-workbench-data ;;
 
-5) sudo apt-get install mysql-workbench mysql-workbench-data ;;
-
-6) sudo apt-get update;
-sudo apt-get update;
-sudo apt-get install python-software-properties;
-sudo apt-get update;
-sudo apt-get install php5;
-sudo apt-get update;
-sudo apt-get install php5-mysql;
-sudo apt-get install pdo-mysql;
-sudo apt-get install curl libcurl3 libcurl3-dev php5-curl;
-sudo apt-get install php5-gd;
-sudo apt-get update;
+5) sudo apt update;
+sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade;
+sudo apt-get install software-properties-common curl
+sudo apt install php7.0-mysql;
+sudo apt install curl libcurl3 libcurl3-dev php7.0-curl;
+sudo apt install php7.0-gd;
+sudo apt update;
 service apache2 reload ;;
 
-7) sudo add-apt-repository ppa:ubuntu-wine/ppa
- sudo apt-get update
- sudo apt-get install wine1.7 winetricks  ;;
-#Para remover o wine1.7
-#sudo apt-get remove wine1.7 winetricks 
-# Para remover completamente:
-#sudo apt-get remove wine
-#rm -rf $HOME/.wine
-#rm -f $HOME/.config/menus/applications-merged/wine*
-#rm -rf $HOME/.local/share/applications/wine
-#rm -f $HOME/.local/share/desktop-directories/wine*
-#rm -f $HOME/.local/share/icons/????_*.xpm 
-8) sudo add-apt-repository ppa:otto-kesselgulasch/gimp;
-sudo apt-get update;
-sudo apt-get install gimp ;;
+6) sudo add-apt-repository ppa:ubuntu-wine/ppa
+ sudo apt update
+ sudo apt install wine1.7 winetricks  ;;
 
-9) sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder;
-sudo apt-get update;
-sudo apt-get install simplescreenrecorder;
-sudo apt-get install simplescreenrecorder-lib:i386 ;;
+7) sudo add-apt-repository ppa:otto-kesselgulasch/gimp;
+sudo apt update;
+sudo apt install gimp ;;
 
-10) sudo add-apt-repository ppa:sunab/kdenlive-release && sudo apt-get update && sudo apt-get install kdenlive ;;
+8) sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder;
+sudo apt update;
+sudo apt install simplescreenrecorder;
+sudo apt install simplescreenrecorder-lib:i386 ;;
 
-11) sudo add-apt-repository ppa:audacity-team/daily;
-sudo apt-get update;
-sudo apt-get install audacity ;;
+9) sudo add-apt-repository ppa:sunab/kdenlive-release && sudo apt update && sudo apt install kdenlive ;;
 
-12) cd ~/Downloads;
+10) sudo add-apt-repository ppa:audacity-team/daily;
+sudo apt update;
+sudo apt install audacity ;;
+
+11) cd ~/Downloads;
 wget http://ufpr.dl.sourceforge.net/project/natron/Natron_Linux_install_bundle_x86-64bit_v0.9.6.tar;
 tar -xf Natron_Linux_install_bundle_*_v0.9.6.tar;
-sudo apt-get install libegl1-mesa;
+sudo apt install libegl1-mesa;
 sudo ./Natron_Linux_install_bundle_*_v0.9.6 ;;
 
-13) sudo add-apt-repository ppa:openshot.developers/ppa;
-sudo apt-get update;
-sudo apt-get install openshot openshot-doc ;;
+12) sudo add-apt-repository ppa:openshot.developers/ppa;
+sudo apt update;
+sudo apt install openshot openshot-doc ;;
 
-14) sudo apt-get install ppa-purge;
+13) sudo apt install ppa-purge;
 sudo add-apt-repository ppa:xorg-edgers/ppa;
 sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt-get update
-sudo apt-get install nvidia-358 nvidia-prime;;
-#sudo apt-get install nvidia-340 ;;
+sudo apt update
+sudo apt install nvidia-358 nvidia-prime;;
+#sudo apt install nvidia-340 ;;
 #remover o driver
 #sudo ppa-purge ppa:xorg-edgers/ppa
-15) sudo add-apt-repository ppa:webupd8team/popcorntime;
-sudo apt-get update; sudo apt-get upgrade;
-sudo apt-get install popcorn-time ;;
+14) sudo add-apt-repository ppa:webupd8team/popcorntime;
+sudo apt update; sudo apt upgrade;
+sudo apt install popcorn-time ;;
 
-16) sudo add-apt-repository ppa:libretro/stable -y && sudo apt-get update && sudo apt-get install retroarch retroarch-* libretro-* -y ;;
+15) sudo add-apt-repository ppa:libretro/stable -y && sudo apt update && sudo apt install retroarch retroarch-* libretro-* -y ;;
 
-17) sudo apt-get install git;;
+16) sudo apt install git;;
 
-171) sudo apt-get install python-software-properties
+17) sudo apt install python-software-properties
 sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs npm ;;
+sudo apt update
+sudo apt install nodejs npm ;;
 
 18) curl -sS https://getcomposer.org/installer | php ;;
+
 19) sudo add-apt-repository ppa:webupd8team/java ;
-sudo apt-get update;
-sudo apt-get install oracle-java8-installer ;;
-20) sudo add-apt-repository ppa:mc3man/trusty-media -y && sudo apt-get update && sudo apt-get install ffmpeg gstreamer0.10-ffmpeg -y;;
-21) sudo apt-get install libavcodec-extra-54;
+sudo apt update;
+sudo apt install oracle-java8-installer ;;
+
+20) sudo add-apt-repository ppa:mc3man/trusty-media -y && sudo apt update && sudo apt install ffmpeg gstreamer0.10-ffmpeg -y;;
+
+21) sudo apt install libavcodec-extra57;
 sudo add-apt-repository ppa:jon-severinsson/ffmpeg;
 sudo add-apt-repository ppa:noobslab/apps;
-sudo apt-get update;
-sudo apt-get install formatjunkie;;
+sudo apt update;
+sudo apt install formatjunkie;;
 
 q) exit ;;
 esac
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 <h1 align="center">
-    <img alt="Caroussel 3D" src="https://github.com/jeffersonvidal/apps-install-shellscript/blob/master/screenshots/screen1.PNG?raw=true"  width="400px"/>
+    <img alt="Apps Install Shell Script" src="./screenshots/logo.png"  width="400px"/>
 </h1>
 
 <h3 align="center" >
-  Apps Install
+  Apps Install Shell Script
 </h3>
 
 <p align="center">
@@ -43,27 +43,77 @@
 </p>
 
 <p align="center">
-  <a href="#environment">Caroussels Types</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#languages">Languages</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-  <a href="#screenshots">Screenshots</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#apps">Apps List</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#about">Apps Functions</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#use">How to use</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
   <a href="#license">License</a>
 </p>
-
-<a id="environment"></a>
-## Caroussels Types
-
-- [x] Croussel 3D horizontal;
-- [x] Croussel 3D vertical;
 
 <a id="languages"></a>
 ## Languages
 
-- [x] HTML5;
-- [x] CSS;
-- [x] Javascript;
+- [x] ShellScript;
 
-<a id="screenshots"></a>
-## Screenshots
+
+
+<a id="apps"></a>
+## Apps list
+
+- [x] System update;
+- [x] Build Essential;
+- [x] MySQL ?;
+- [x] MySQL Workbench;
+- [x] PHP 7.2 + Apache2;
+- [x] Git;
+- [x] Wine;
+- [x] Gimp;
+- [x] Simple Screen Recorder;
+- [x] Kdenliver;
+- [x] Audacity;
+- [x] Driver de Video nVidia GeForce GT 740M;
+- [x] PopCorn Time;
+- [x] RetroArch;
+- [x] NodeJS;
+- [x] Composer;
+- [x] Java 8;
+- [x] Codec FFMPEG;
+- [x] Format Junkie;
+
+<a id="about"></a>
+## Apps Functions
+
+- [x] System update - Checks and installs existing updates for the system;
+- [x] Build Essential - Installs essential system tools;
+- [x] MySQL - Installs MySQL database in the latest version;
+- [x] MySQL Workbench - Is a DBMS database for MySQL and MariaDB;
+- [x] PHP 7 + Apache2 - Programming language for web applications;
+- [x] Git - Works with versioning. Perfect for working with collaborators simultaneously on a single project;
+- [x] Wine - "Emulates" Windows environment within Linux and allows installation of some Windows applications on Linux;
+- [x] Gimp - Free image editor, similar to Photoshop;
+- [x] Simple Screen Recorder - Screen recorder. Ideal for recording video lessons or screencasts;
+- [x] Kdenliver - Free video editor. Ideal for editing video lessons or videos for Youtube;
+- [x] Audacity - Audio editor. Removes noise, amplifies and other effects and features;
+- [x] Driver de Video nVidia GeForce GT 740M -NVidia video card driver GT740M;
+- [x] PopCorn Time - Application similar to Netflix, allows you to watch videos through streaming and torrent files;
+- [x] RetroArch - Application that allows you to install old Nintendo games, Atari among others;
+- [x] NodeJS - NodeJS instalation;
+- [x] Composer - Used to install packages via JSON using PHP;
+- [x] Java 8 - Java 8 instalation;
+- [x] Codec FFMPEG - Video codecs pack;
+- [x] Format Junkie - Video converter for several extensions;
+
+<a id="use"></a>
+## How to use
+
+<p align="center">
+
+1 - In the terminal, go to the folder of the file AppsInstall.sh, type the command "chmod 777 AppsInstall.sh" and press the "Enter" key to give the file execution permission.
+2 - Then type "./AppsInstall.sh" and press the "Enter" key then the options menu will appear. 
+3 - Simply enter the number in front of each option to install the desired item.
+4 - To exit the script type "q" and press the "Enter" key.
+
+</p>
 
 <p align="center">
 
